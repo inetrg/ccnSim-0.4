445c1425797a3c33ca1fd9c84e7252d509b385e8
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2021-03-25 13:20:34 +0100

    makemake.sh: add build lib option

--- scripts/makemake.sh
+++ scripts/makemake.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-opp_makemake --deep -f -X  ./patch/   -X scripts/ -X networks/ -X modules/  -o ccnSim -X results/ -X ini/ -X manual/  -X doc/ -X file_routing/ -X ccn14distrib/ -X ccn14scripts/ -I include/ -I packets/
+#!/bin/bash
+if [[ (${BUILD_LIB} == 1) ]]; then
+    OPP_MAKEMAKE_ARGS=-s
+fi
+opp_makemake --deep -f -X  ./patch/   -X scripts/ -X networks/ -X modules/  -o ccnSim -X results/ -X ini/ -X manual/  -X doc/ -X file_routing/ -X ccn14distrib/ -X ccn14scripts/ -I include/ -I packets/ ${OPP_MAKEMAKE_ARGS}
